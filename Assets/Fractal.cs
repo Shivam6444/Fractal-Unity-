ffacbfc27af88c31fbe7f69511459cb1cd7e49d3
Shivam6444 <33787741+Shivam6444@users.noreply.github.com>
2018-05-20 23:25:43 -0300

    Twist

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -3,29 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fractal : MonoBehaviour {
-//Material and mesh are like Classes.
-    
-    public Mesh mesh;//Contains shapes like triangles.
-    public Material material;//Contains Colors and stuff.
+    //Material and mesh are like Classes.MESH//Contains shapes like triangles.Material//Contains Colors and stuff.
+
+    public Mesh[] meshes;
+    public Material material;
     public int maxDepth;
     private int depth;
+    public float maxTwist;
     public float childScale;
-
+    public float spawnProbability;
     private Material[,] materials;
+    public float maxRotationSpeed;
+    private float rotationSpeed;
 
-    private void InitializeMaterials(){
-        materials = new Material[maxDepth + 1, 2];
-        for (int i = 0; i <= maxDepth; i++){
-            float t = i / (maxDepth - 1f);
-            t *= t;
-            materials[i, 0] = new Material(material);
-            materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
-            materials[i, 1] = new Material(material);
-            materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
-        }
-        materials[maxDepth, 0].color = Color.magenta;
-        materials[maxDepth, 1].color = Color.red;
-    }
+
     private static Vector3[] childDirections = {
         Vector3.up,
         Vector3.right,
@@ -41,12 +32,16 @@ public class Fractal : MonoBehaviour {
         Quaternion.Euler(90f, 0f, 0f),
         Quaternion.Euler(-90f, 0f, 0f)
     };
-
+    private void Update(){
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
     private void Start(){//invoked by unity once its component is enabled.
+        rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+        transform.Rotate(Random.Range(-maxTwist, maxTwist), 0f, 0f);
         if (materials == null){//If materails have nothing inside initialize it(by using that method).
             InitializeMaterials();
         }
-        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
         gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range(0,2)];
         //GetComponent<MeshRenderer>().material.color = Color.Lerp(Color.white, Color.yellow, (float)depth / maxDepth);
         if (depth < maxDepth){
@@ -54,8 +49,12 @@ public class Fractal : MonoBehaviour {
         }// HERE THIS MEANS the current struct in which "this" is present.
 
     }
+
     private void Initialize(Fractal parent, int childIndex){//This method will invoke before Start.
-        mesh = parent.mesh;
+        meshes = parent.meshes;
+        maxTwist = parent.maxTwist;
+        maxRotationSpeed = parent.maxRotationSpeed;
+        spawnProbability = parent.spawnProbability;
         materials = parent.materials;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
@@ -72,9 +71,26 @@ public class Fractal : MonoBehaviour {
     private IEnumerator CreateChildren(){
         //for loop will itterate through the number of direction childDirections struct have
         for (int i = 0; i < childDirections.Length; i++){
-            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));//<-Giving time and after that time, it is performing task.
-            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, i);
+            if (Random.value < spawnProbability){
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));//<-Giving time and after that time, it is performing task.
+                new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, i);
+            }
         }
+    }
+
+    private void InitializeMaterials(){
+        materials = new Material[maxDepth + 1, 2];
+        for (int i = 0; i <= maxDepth; i++)
+        {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i, 0] = new Material(material);
+            materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+            materials[i, 1] = new Material(material);
+            materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+        }
+        materials[maxDepth, 0].color = Color.magenta;
+        materials[maxDepth, 1].color = Color.red;
     }
 
 
